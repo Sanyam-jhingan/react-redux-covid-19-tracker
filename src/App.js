30f9e3154e801c6e1357d6429f52ffdb528cf109
sanyam <sanyam.jhingan1.19@mountblue.tech>
2022-04-15 14:46:27 +0530

    add loading skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { GridContainer } from './components/GridContainer';
+
+import AppBar from "./components/AppBar"
+
+//import Map from "./components/Map";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar />
+      <GridContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
+import countriesSlice from "./features/CountriesReducer"
+import CountrySlice from "./features/CountryReducer"
+import loadingSlice from "./features/LoadingReducer"
+const store = configureStore({
+  reducer: {
+    countries: countriesSlice,
+    country: CountrySlice,
+    loading: loadingSlice,
+  },
+})
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
